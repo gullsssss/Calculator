bb1ff11d1a3b005fb8933c5ebe7ef88e8fbf20b3
gullsssss <kapustaa356@gmail.com>
2026-03-14 18:55:28 +0200

    Correct division logic in calculator.py
    Fix division operation to handle division by zero correctly.

--- calculator.py
+++ calculator.py
@@ -15,10 +15,11 @@ if c == "*":
 	result=(a*b)
     print("Результат: ", result)
 if c == "/":
-	result=(a/b)
-    print("Результат: ", result)
     if b == "0":
 		print("На 0 делить нельзя")
+	else:
+		result = a/b
+		print("Результат", result)
     
 if c == "**":
 	result=(a**b)
